8e4b0ef2ffd372f07aa70bec6a6a9b2475e3e637
unknown <Sala300@TUNDFPCGE10F360.FormacionSena.RED>
2019-08-06 15:10:00 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace proyectoSenova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            int puntaje = 0;
+            Proyecto miProyecto = new Proyecto();
+            Proyecto miProyecto2 = new Proyecto();
+            Proyecto miProyecto3 = new Proyecto();
+            Proyecto miProyecto4 = new Proyecto("Realidad aumentada", 12540, "sistemas", 10);
+
+            //miProyecto3.IngresarProyecto();
+            //miProyecto3.IngresarProyecto();
+            miProyecto4.ImprimirProyecto();
+
+            //miProyecto.area = "sistemas";
+            //miProyecto.codigo = 12000;
+            //miProyecto.duracion = 13;
+            //miProyecto.nombre = "desarrollo internet de las cosas IOT";
+            //miProyecto2.area = "redes";
+            //miProyecto2.codigo = 13000;
+            //miProyecto2.duracion = 14;
+            //miProyecto2.nombre = "desarrollo arduino";
+            //Console.WriteLine($"area:{miProyecto.area}");
+            //Console.WriteLine($"area:{miProyecto2.area}");
+            //Console.WriteLine($"codigo:{miProyecto.codigo}");
+            //Console.WriteLine($"codigo:{miProyecto2.area}");
+            //Console.WriteLine($"duracion:{miProyecto.duracion}");
+            //Console.WriteLine($"duracion:{miProyecto2.area}");
+            //Console.WriteLine($"nombre:{miProyecto.nombre}");
+            //Console.WriteLine($"nombre:{miProyecto2.area}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Proyecto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyectoSenova
+{
+    public class Proyecto
+    {
+        #region Atributos
+
+        #endregion
+        #region Propiedades
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+        #endregion
+
+        #region contructores
+        public Proyecto(string nombre, double codigo, string area, int duracion)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+        }
+        public Proyecto()
+        {
+
+        }
+        public Proyecto(string area, int duracion)
+        {
+            Area = area;
+            Duracion = duracion;
+
+        }
+        #endregion
+
+        #region metodos
+        public void IngresarProyecto()
+        {
+            Console.WriteLine("Ingrese el nombre del proyecto");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el codigo");
+            Codigo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el area del proyecto");
+            Area = Console.ReadLine();
+            Console.WriteLine("Ingrese la duracion del proyecto");
+            var dura = Console.ReadLine();
+            Duracion = int.Parse(dura);
+
+        }
+        public void ImprimirProyecto()
+        {
+            Console.WriteLine($"el nombre del proyecto es:{Nombre}");
+            Console.WriteLine($"el codigo del proyecto es:{Codigo}");
+            Console.WriteLine($"la duracion del proyecto es:{Duracion}");
+            Console.WriteLine($"el area del proyecto es:{Area}");
+        }
+
+
+        #endregion
+    }
+}
